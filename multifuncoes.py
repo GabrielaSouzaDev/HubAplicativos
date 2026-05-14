4ecb18c1f6255a791fed4433e9c24d42ff424975
Gabriela <mr27612@gmail.com>
2026-05-14 20:44:42 -0300

    conclusão inicial

--- multifuncoes.py
+++ multifuncoes.py
@@ -15,10 +15,26 @@ app.title('MULTIFUNÇÕES')
 app.iconbitmap('tools.ico')
 
 # frame esquerda/menu
+frame_menu = ctk.CTkFrame(app,
+                          width=200,
+                          height=600,
+                          fg_color= '#FFE880')
+frame_menu.pack(side='left',fill='y')
+
 
 # frame direita/conteúdo
+frame_conteudo = ctk.CTkFrame(app,
+                              fg_color='#964B00')
+frame_conteudo.pack(side='right', fill='both', expand='True')
 
 # botões
+ctk.CTkButton(frame_menu,
+              fg_color='#FFFFFF',
+              text_color=('#0F0F0F'),
+              hover_color=('#964B00'),
+              corner_radius=12,
+              text='Juros Simples').pack(pady=10, padx=10)
+
 
 # loop
 app.mainloop()
