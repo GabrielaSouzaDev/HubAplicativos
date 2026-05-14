84da406f3b951532c81d3d9587b30309643a8db1
Gabriela <mr27612@gmail.com>
2026-05-14 19:35:43 -0300

    tema e janela

--- multifuncoes.py
+++ multifuncoes.py
@@ -1,3 +1,24 @@
 # Autor: Gabriela Souza
-# Projeto em Python de Ferramentas Essenciais
+# Projeto - Ferramentas Essenciais
 
+import customtkinter as ctk
+import requests
+
+# configuração visual
+ctk.set_appearance_mode('dark')
+ctk.set_default_color_theme('dark-blue')
+
+# janela principal
+app = ctk.CTk()
+app.geometry('800x600')
+app.title('MULTIFUNÇÕES')
+app.iconbitmap('tools.ico')
+
+# frame esquerda/menu
+
+# frame direita/conteúdo
+
+# botões
+
+# loop
+app.mainloop()
